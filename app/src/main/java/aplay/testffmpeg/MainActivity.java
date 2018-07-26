62d2cb1425d071fcb491c65e52b2de843b2e3530
lineagech <lineagech@gmail.com>
2018-07-26 23:48:37 +0800

    Resolve Screen Landscape display problem

--- app/src/main/java/aplay/testffmpeg/MainActivity.java
+++ app/src/main/java/aplay/testffmpeg/MainActivity.java
@@ -1,7 +1,10 @@
 package aplay.testffmpeg;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        // Remove Title
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Full Screen
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+
+        // Horizontal
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE );
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById( R.id.sample_text );
